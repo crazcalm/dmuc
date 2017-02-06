2189713372c4f2a4aead494ac70684f6c29f521e
Marcus Willock <crazcalm@gmail.com>
2017-02-06 18:48:37 -0500

    first attempt at the grepCall func

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"strings"
-	"os/exec"
 	"os"
-	"bytes"
+	"os/exec"
+	"strings"
 )
-
 
 var l = flag.Bool("l", false, "List files from /usr/local/bin directory")
 var a = flag.Bool("a", false, "List files from both /usr/bin and /usr/local/bin directory")
+var grep1 = flag.String("s", "", "Applies a grep 'char' filter to the output")
+var grep2 = flag.String("i", "", "Apllies a grep 'string' filter to the output")
 
-func lsCall(paths string, output bytes.Buffer){
+func grepCalls(paths string, output bytes.Buffer, grepString string, charGrep bool) string {
+	args := strings.Split(paths, " ")
+	var grepArg string
+	var cmd *exec.Cmd
+
+	if charGrep {
+		grepArg = fmt.Sprintf("^%s", grepString)
+	} else {
+		grepArg = grepString
+	}
+	
+	if len(args) == 1 {
+		cmd = exec.Command("ls", args[0], "grep", grepArg)
+	}else{
+		cmd = exec.Command("ls", args[0], args[1], "grep", grepArg)
+	}
+	cmd.Stdout = &output
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: ", err)
+		os.Exit(1)
+	}
+	return output.String()
+}
+
+func lsCall(paths string, output bytes.Buffer) string {
 	args := strings.Split(paths, " ")
 	var cmd *exec.Cmd
 	if len(args) == 1 {
@@ -27,11 +53,14 @@ func lsCall(paths string, output bytes.Buffer){
 		fmt.Fprintf(os.Stderr, "error: ", err)
 		os.Exit(1)
 	}
-	fmt.Print(output.String())
+	return output.String()
 }
 
+func printToScreen(content string) {
+	fmt.Print(content)
+}
 
-func main(){
+func main() {
 	cmd := exec.Command("ls", "/usr/bin/", "/usr/local/bin")
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -40,9 +69,9 @@ func main(){
 		fmt.Fprintf(os.Stderr, "error: ", err)
 		os.Exit(1)
 	}
-	fmt.Print(out.String())
-	
+	printToScreen(out.String())
+
 	flag.Parse()
-	var out2  bytes.Buffer
-	lsCall("/usr/bin", out2)
+	var out2 bytes.Buffer
+	printToScreen(lsCall("/usr/bin", out2))
 }
