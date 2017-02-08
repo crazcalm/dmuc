87c92e3535a0e92aedfbd43eab5a51902c0e87c6
Marcus Willock <crazcalm@gmail.com>
2017-02-08 07:11:44 +0000

    Basic functionality is done

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"os/exec"
 	"log"
+	"os/exec"
 )
 
 const (
-	userBin = "/usr/bin/"
+	userBin      = "/usr/bin/"
 	userLocalBin = "/usr/local/bin/"
-	grep = "grep"
-	LS = "ls"
+	grep         = "grep"
+	LS           = "ls"
 )
 
 var l = flag.Bool("l", false, "List files from /usr/local/bin directory")
@@ -20,12 +20,12 @@ var a = flag.Bool("a", false, "List files from both /usr/bin and /usr/local/bin 
 var s = flag.String("s", "", "Applies a grep 'char' filter to the output")
 var i = flag.String("i", "", "Apllies a grep 'string' filter to the output")
 
-func createBashCommand(local *bool, all *bool, grepStartsWith *string, grepIncludes* string) ([]string, []string) {
+func createBashCommand(local *bool, all *bool, grepStartsWith *string, grepIncludes *string) ([]string, []string) {
 	lsArgs := []string{LS}
-	if *all{
+	if *all {
 		lsArgs = append(lsArgs, userBin)
 		lsArgs = append(lsArgs, userLocalBin)
-	}else if *local {
+	} else if *local {
 		lsArgs = append(lsArgs, userLocalBin)
 	} else {
 		lsArgs = append(lsArgs, userBin)
@@ -33,29 +33,28 @@ func createBashCommand(local *bool, all *bool, grepStartsWith *string, grepInclu
 
 	grepArgs := []string{grep}
 
-	if *grepStartsWith != "" || *grepIncludes != ""{
-		if *grepStartsWith != ""{
+	if *grepStartsWith != "" || *grepIncludes != "" {
+		if *grepStartsWith != "" {
 			grepArgs = append(grepArgs, fmt.Sprintf("^%s", *grepStartsWith))
 		} else {
 			grepArgs = append(grepArgs, fmt.Sprintf("%s", *grepIncludes))
 		}
 	}
 	return lsArgs, grepArgs
-	
+
 }
 
-func errorHandler(err error){
-	if err !=nil {
+func errorHandler(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func runCommand(lsArgs []string, grepArgs []string, output bytes.Buffer) {
+func runCommand(lsArgs []string, grepArgs []string, output bytes.Buffer) string {
 	var lsCmd *exec.Cmd
 	var grepCmd *exec.Cmd
-	
+
 	numOfArgs := len(lsArgs)
-	fmt.Printf("num of args: %d\n", numOfArgs)
 
 	if numOfArgs == 2 {
 		lsCmd = exec.Command(lsArgs[0], lsArgs[1])
@@ -74,36 +73,37 @@ func runCommand(lsArgs []string, grepArgs []string, output bytes.Buffer) {
 		errorHandler(err)
 
 		err = lsCmd.Start()
-		fmt.Print("run lsCmd")
 		errorHandler(err)
 
 		err = lsCmd.Wait()
 		errorHandler(err)
-		
-		err = grepCmd.Wait()
-		fmt.Print("grepCmd")
-		errorHandler(err)
+
+		// There is an error when the output is empty
+		//So... ignore it?
+		_ = grepCmd.Wait()
 	} else {
 		lsCmd.Stdout = &output
 		err := lsCmd.Run()
 		errorHandler(err)
 	}
 
-	fmt.Print("output: ")
-	fmt.Println(output.String())
-
+	return output.String()
 }
 
 func printToScreen(content string) {
-	fmt.Println(content)
-	fmt.Println("over")
+	fmt.Println("See Results Below: ")
+	if content == "" {
+		fmt.Println("None")
+	} else {
+		fmt.Println(content)
+	}
 }
 
 func main() {
 	flag.Parse()
 
 	var output bytes.Buffer
-	lsArgs, grepArgs := createBashCommand(l,a,s,i)
-	runCommand(lsArgs, grepArgs, output)
-	printToScreen(output.String())
+	lsArgs, grepArgs := createBashCommand(l, a, s, i)
+	result := runCommand(lsArgs, grepArgs, output)
+	printToScreen(result)
 }
